1c188e7781572f02974da6490d14858ef2336ca3
Simon Ker <simon@eyetoi.net>
2013-05-22 11:36:28 +0200

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedWineReviews::Application.config.secret_token = '1226149e4d17c055e334a9436ff027ba5237a2436c702023298afad28756ddf22a829b3d3d9adcb99252cde5478108a766a207a6a11a16137a633581656c4364'
